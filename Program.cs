0c8c73ceb926e3b18b20f7b7963bcd5732370977
Sivamuthu Kumar <ksivamuthu@ceiamerica.com>
2019-04-22 21:28:24 -0400

    Added device client and registry client

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Devices.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace iot_app_docker
 {
@@ -14,6 +18,7 @@ namespace iot_app_docker
     {
         public static void Main(string[] args)
         {
+            RegisterDeviceClient();
             CreateWebHostBuilder(args).Build().Run();
         }
 
@@ -21,5 +26,54 @@ namespace iot_app_docker
             WebHost.CreateDefaultBuilder(args)
                 .UseUrls("http://0.0.0.0:5000")
                 .UseStartup<Startup>();
+
+        private static string connectionString = "HostName=siva-iot-hub.azure-devices.net;DeviceId=smart-device-1;SharedAccessKey=PIwuifCs+4mU1yJT26yf31iXN1A1ktoOEHKjswoPqmM=";
+        private static DeviceClient deviceClient;
+
+        private static void RegisterDeviceClient()
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
+            deviceClient.SendEventAsync(new Message(Encoding.UTF8.GetBytes("IoTAppStarted")));
+
+            StartReceiveMessages();
+
+            StartTemperatureTelemetry();
+        }
+
+        private static Timer temperatureSensorTimer;
+        private static void StartTemperatureTelemetry()
+        {
+            temperatureSensorTimer = new Timer((state) => { SendTemperatureTelemetry(); }, null, 2000, 5 * 1000);
+        }
+
+        private static void SendTemperatureTelemetry()
+        {
+            var data = new { type = "temperature", value = 70.5 };
+            var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data)));
+            deviceClient.SendEventAsync(message);
+        }
+
+        private static Timer receiveMessageTimer;
+
+        private static void StartReceiveMessages()
+        {
+            receiveMessageTimer = new Timer(async (state) => {
+                try
+                {
+                    Message receivedMessage = await deviceClient.ReceiveAsync();
+                    if (receivedMessage == null) return;
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Received message: {0}",
+                    Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                    Console.ResetColor();
+
+                    await deviceClient.CompleteAsync(receivedMessage);
+                }catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }, null, 0, 1000);
+        }
     }
 }
--- TwitchNotes.md
+++ TwitchNotes.md
@@ -24,3 +24,33 @@ Device 2: sivaiot.registry.io/iot-app:2
 
 Docker is not building - or not reporting progress (hanging). Restart of the machine will work
 
+## April 19, 2019
+Docker commands - (port mapping didn't work) 
+Then realized port mapping command is wrong
+
+## April 22, 2019
+
+Planned:
+IoT Hub is connected with .net core application
+Able to send message (Telemetry - Temperature, sensor data)
+Able to receive message (IoT Firmware update)
+Build this docker, CI/CD, push to azure container registry
+
+## Done
+1. Added IoT Device Client SDKs
+2. Added telemetry temperature sensor
+3. We are able to receive cloud messages on device
+
+## Postponed
+Build this docker, CI/CD, push to azure container registry
+
+## April 24, 2019
+Planned:
+Build this docker, CI/CD, push to azure container registry
+Setup the watchtower
+Wireup the docker pull code from Azure IoT hub messages.
+
+## Future
+Planned:
+Deploying this in raspberry device
+Demo - Update using docker container
